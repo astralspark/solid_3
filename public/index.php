c5749ad93361d51656747b1d0a4f0d45b7c67d86
Lechu <ginstead@gmail.com>
2018-07-01 23:26:33 +0200

    First commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+
+<title>SOLID prototyp</title>
+<meta charset="utf-8">
+<meta name="robots" content="none">
+
+<body>
+
+Jest to niedziałający, uproszczony prototyp frameworku, <strong>niewykorzystywany w żadnym z projektów</strong>. Jego celem, jest jedynie prezentacja umiejętności pracy, z wykorzystanem zasad projektowych SOLID.
